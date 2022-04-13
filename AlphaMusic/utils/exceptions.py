583c4d7411a101f958e79e78b85103c9c23091b3
theend-alpha <101687704+theend-alpha@users.noreply.github.com>
2022-04-13 11:41:21 +0530

    Create exceptions.py

---
+++ AlphaMusic/utils/exceptions.py
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021-2022 by theend-alpha@Github, < https://github.com/theend-alpha >.
+#
+# This file is part of < https://github.com/theend-alpha/AlphaMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/theend-alpha/AlphaMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+
+class AssistantErr(Exception):
+    def __init__(self, errr: str):
+        super().__init__(errr)
