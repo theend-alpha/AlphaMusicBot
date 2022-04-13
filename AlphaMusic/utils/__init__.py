020ccc9fc62f08425fbd9155adf7dba18e6ef39f
theend-alpha <101687704+theend-alpha@users.noreply.github.com>
2022-04-13 11:39:11 +0530

    Create __init__.py

---
+++ AlphaMusic/utils/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2021-2022 by theend-alpha@Github, < https://github.com/theend-alpha >.
+#
+# This file is part of < https://github.com/theend-alpha/AlphaMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/theend-alpha/AlphaMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .formatters import *
+from .inline import *
+from .pastebin import *
+from .sys import *
