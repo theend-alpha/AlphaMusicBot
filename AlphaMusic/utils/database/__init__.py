3aa4984d78853ec4002bf2a8085af9253845c0c2
theend-alpha <101687704+theend-alpha@users.noreply.github.com>
2022-04-13 11:48:15 +0530

    Create __init__.py

---
+++ AlphaMusic/utils/database/__init__.py
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021-2022 by theend-alpha@Github, < https://github.com/theend-alpha >.
+#
+# This file is part of < https://github.com/theend-alpha/AlphaMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/theend-alpha/AlphaMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .assistantdatabase import *
+from .memorydatabase import *
+from .mongodatabase import *
